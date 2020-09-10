189c0880c1144c295444118d4683aab6d2c79f8c
allenliuzihao <allenliuzihao@gmail.com>
2020-09-10 01:15:59 -0700

    some minor comments

--- Renderer.cpp
+++ Renderer.cpp
@@ -234,7 +234,7 @@ VkShaderModule Renderer::createShaderModule(const std::vector<char>& code){
     VkShaderModuleCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
     createInfo.codeSize = code.size();
-    createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+    createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());          // no alignment issue since code.data() is 32 bit aligned by vector class.
     VkShaderModule shaderModule;
     if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
         throw std::runtime_error("failed to create shader module!");
