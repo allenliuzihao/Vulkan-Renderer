ae75f96c900563993c75333d04c544d4cdf5bd64
allenliuzihao <allenliuzihao@gmail.com>
2020-09-21 14:59:41 -0700

    don't care about the color buffer because it is not needed after the renderpass for presentation

--- Renderer.cpp
+++ Renderer.cpp
@@ -416,7 +416,7 @@ void Renderer::createRenderPass(){
     colorAttachment.format = swapchainImageFormat;
     colorAttachment.samples = msaaSamples;
     colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-    colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
     colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
     colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
     colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
